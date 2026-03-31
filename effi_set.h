b221f0d348ef6ed24ae083b02442c7a811d44398
Gabriel <gabrieltoro45@tamu.edu>
2026-03-30 19:14:48 -0500

    added parallel DIB array; prototyped a packed version of this class

--- effi_set.h
+++ effi_set.h
@@ -3,60 +3,115 @@
 #include <functional>
 #include <string>
 
-template <typename T, int precision> 
-class effi_set {
-    private:
-        // std::conditional for compile-time type determination
-        using width_type = 
-            std::conditional_t<precision <= 8,   uint8_t,
-            std::conditional_t<precision <= 16,  uint16_t,
-            std::conditional_t<precision <= 32,  uint32_t, uint64_t>>>;
-        
-        width_type* array;                      // the internal set array
-        int _size;                              // the set's size visible to the user
-        int _capacity;                          // current set capacity
-        width_type (*hash_logic)(const T&);     // function pointer to the hash function used by the set 
-        
+namespace effi {
+    #define INITIAL_SET_CAP 10
 
-    public:
-        /************  Internal Hash Function ************/
-        static width_type hash_function(const T& object) {
-            width_type _hash = std::hash<T>{}(object);
-            return _hash;
-        }
-        /************  Constructor(s) ************/
-        // Default Constructor
-        effi_set() : array(nullptr), _size(0), _capacity(0), hash_logic(&hash_function) {}
-        
-        // Custom Hash Function Constructor
-        effi_set(width_type (*custom_hash)(const T&)) : array(nullptr), _size(0), _capacity(0), hash_logic(custom_hash) {}
+    template <typename T, int precision> 
+    class effi_set {
+        private:
+            // std::conditional for compile-time type determination
+            using width_type = 
+                std::conditional_t<precision <= 8,   uint8_t,
+                std::conditional_t<precision <= 16,  uint16_t,
+                std::conditional_t<precision <= 32,  uint32_t, uint64_t>>>;
+            
+            // the internal set array
+            width_type* array;
 
-        // Destructor
-        ~effi_set() { delete[] array; }
+            // the metadata array for each element (required by the robinhood algorithm)
+            // this is parallel to the internal array 
+            uint8_t* dibs;
+            
+            // the set's size visible to the user
+            int _size;                              
+            
+            // current set capacity
+            int _capacity;         
 
-        /************  Class Methods ************/
-        int insert(T object) {
-            width_type object_hash = this->hash_logic(object);
-            printf("Object Hash on insert call: %lu\n", object_hash);
-            // TODO
-            return 0;
-        }
+            // function pointer to the hash function used by the set
+            width_type (*hash_logic)(const T&);      
 
-        int contains(T object) {
-            // TODO
-            return 0;
-        }
-        
-        int remove(T object) {
-            // TODO
-            return 0;
-        }
+            // grow the internal array when at/near capacity 
+            void grow() {                           
+                
+            }
+            
 
-        int clear() {
-            // TODO
-            return 0;
-        }
+        public:
+            /************  Internal Hash Function ************/
+            static width_type hash_function(const T& object) {
+                width_type _hash = std::hash<T>{}(object);
+                return _hash;
+            }
 
-        int size() { return this->_size; }
-        int capacity() { return this->_capacity; }
-};
+            /************  Constructor(s) ************/
+            // Default Constructor
+            effi_set() : array(new width_type[INITIAL_SET_CAP]), 
+                        dibs(new uint8_t[INITIAL_SET_CAP])
+                        _size(0), 
+                        _capacity(INITIAL_SET_CAP), 
+                        hash_logic(&hash_function) {}
+            
+            // Custom Hash Function Constructor
+            effi_set(width_type (*custom_hash)(const T&)) : array(new width_type[INITIAL_SET_CAP]), 
+                                                            dibs(new uint8_t[INITIAL_SET_CAP])
+                                                            _size(0), 
+                                                            _capacity(INITIAL_SET_CAP), 
+                                                            hash_logic(custom_hash) {}
+
+            // Destructor
+            ~effi_set() { 
+                delete[] array;
+                delete[] dibs;
+            }
+
+            /************  Class Methods ************/
+            int insert(T object) {
+                // grow internal array at 75% capacity
+                if(this->_size >= this->_capacity * 0.75) {
+                    this->grow();
+                }
+
+                width_type object_hash = this->hash_logic(object);
+                printf("Object Hash on insert call: %lu\n", object_hash);
+                // TODO
+                return 0;
+            }
+
+            bool contains(T object) {
+                // TODO
+                return 0;
+            }
+            
+            int remove(T object) {
+                // TODO
+                return 0;
+            }
+
+            int clear() {
+                // TODO
+                return 0;
+            }
+
+            int size() { return this->_size; }
+            int capacity() { return this->_capacity; }
+    };
+
+
+    // TODO: protoyping so I don't forget
+    // template <typename T, int precision> 
+    // class effi_set_packed {
+    //     private:
+    //         // std::conditional for compile-time type determination
+    //         using width_type = 
+    //             std::conditional_t<precision <= 8,   uint8_t,
+    //             std::conditional_t<precision <= 16,  uint16_t,
+    //             std::conditional_t<precision <= 32,  uint32_t, uint64_t>>>;
+    //             #pragma pack(push, 1)
+    //             struct element {
+    //             // width_type hash : precision - 8,
+    //             // uint8_t pcl
+    //             }
+    //             #pragma pack(pop)
+    // };
+}
