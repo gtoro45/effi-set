77cd0381b5022274178bdc032a79bc19f1bcf70f
Gabriel <gabrieltoro45@tamu.edu>
2026-03-30 22:00:05 -0500

    changed dibs member to PSLs to match GFG terminology

--- effi_set.h
+++ effi_set.h
@@ -18,9 +18,9 @@ namespace effi {
             // the internal set array
             width_type* array;
 
-            // the metadata array for each element (required by the robinhood algorithm)
+            // the Probe Sequence Length (PSL) for each element (required for the robinhood algorithm)
             // this is parallel to the internal array 
-            uint8_t* dibs;
+            uint8_t* PSLs;
             
             // the set's size visible to the user
             int _size;                              
@@ -47,14 +47,14 @@ namespace effi {
             /************  Constructor(s) ************/
             // Default Constructor
             effi_set() : array(new width_type[INITIAL_SET_CAP]), 
-                        dibs(new uint8_t[INITIAL_SET_CAP])
+                        PSLs(new uint8_t[INITIAL_SET_CAP])
                         _size(0), 
                         _capacity(INITIAL_SET_CAP), 
                         hash_logic(&hash_function) {}
             
             // Custom Hash Function Constructor
             effi_set(width_type (*custom_hash)(const T&)) : array(new width_type[INITIAL_SET_CAP]), 
-                                                            dibs(new uint8_t[INITIAL_SET_CAP])
+                                                            PSLs(new uint8_t[INITIAL_SET_CAP])
                                                             _size(0), 
                                                             _capacity(INITIAL_SET_CAP), 
                                                             hash_logic(custom_hash) {}
@@ -62,7 +62,7 @@ namespace effi {
             // Destructor
             ~effi_set() { 
                 delete[] array;
-                delete[] dibs;
+                delete[] PSLs;
             }
 
             /************  Class Methods ************/
@@ -100,18 +100,15 @@ namespace effi {
 
     // TODO: protoyping so I don't forget
     // template <typename T, int precision> 
-    // class effi_set_packed {
+    // class effi_set_packed_64 {
     //     private:
-    //         // std::conditional for compile-time type determination
-    //         using width_type = 
-    //             std::conditional_t<precision <= 8,   uint8_t,
-    //             std::conditional_t<precision <= 16,  uint16_t,
-    //             std::conditional_t<precision <= 32,  uint32_t, uint64_t>>>;
-    //             #pragma pack(push, 1)
-    //             struct element {
-    //             // width_type hash : precision - 8,
-    //             // uint8_t pcl
-    //             }
-    //             #pragma pack(pop)
+    //         // single element of the internal array (4-byte aligned)
+    //         struct element {
+    //                 uint64_t hash : 56;     // 56-bit hash
+    //                 uint64_t pcl : 8;       // 8-bit pcl for Robin Hood
+    //         };
+
+    //     public:
+    //         // TODO
     // };
 }
